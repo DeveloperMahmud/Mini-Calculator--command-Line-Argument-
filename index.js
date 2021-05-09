85abcf4cfe8511025ca61ba915ccb4de62dcbb6c
prince <mrepte700@gmail.com>
2021-05-10 01:13:58 +0600

    Add final code (if/else if/else) to complete

--- index.js
+++ index.js
@@ -7,3 +7,18 @@ const sub = require('./calculator/sub');
 const mul = require('./calculator/mul');
 const div = require('./calculator/div');
 
+const a = parseInt(process.argv[2]);
+const choice = process.argv[3];
+const b = parseInt(process.argv[4]);
+
+if (choice === 'ADD'){
+    console.log(add(a,b));
+}else if(choice === 'SUB'){
+    console.log(sub(a,b));
+}else if(choice === 'MUL'){
+    console.log(mul(a,b));
+}else if(choice === 'DIV'){
+    console.log(div(a,b));
+}else{
+    console.log('Invalid Inputs!');
+}
