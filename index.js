35d3ba7e8bcbe3ad70728c9a75b7d2a8c1e71ba3
prince <mrepte700@gmail.com>
2021-05-10 00:28:41 +0600

    import all functions using common js

---
+++ index.js
@@ -0,0 +1,9 @@
+//Mini Calcualtor
+//Functionality : ADD / SUB / MUL / DIV
+//Take input data from command line
+
+const add = require('./calculator/add');
+const sub = require('./calculator/sub');
+const mul = require('./calculator/mul');
+const div = require('./calculator/div');
+
